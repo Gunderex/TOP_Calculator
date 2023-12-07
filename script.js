70f30b5c7e84fe2171032523a9dffa0f1dcf8221
Gunderex <125839591+Gunderex@users.noreply.github.com>
2023-12-07 13:36:53 -0700

    Added operate function to run the equations repurposed simpler versions of the scripts used in the calculator exercise cleaned up to use universal inputs num1 and num2.
    created if else statement to run specific operations depending on which
    operator is used.

---
+++ script.js
@@ -0,0 +1,25 @@
+let result = "";
+
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        const add = function(num1, num2) {
+            result = num1 + num2;
+            return result;
+        };
+    } else if (operator == "-") {
+        const subtract = function(num1, num2) {
+            result = num1 - num2;
+            return result;
+        };
+    } else if (operator == "*") {
+        const multiply = function(num1, num2) {
+            result = num1 * num2;
+            return result;
+        };
+    } else if (operator == "/") {
+        const divide = function(num1, num2) {
+            result = num1 / num2;
+            return result;
+        };
+    }
+}
