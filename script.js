e9b030986b427bc2533fa162f3dfe30cf7b9243f
Gunderex <125839591+Gunderex@users.noreply.github.com>
2023-12-07 14:13:11 -0700

    working on button listeners working on having separate button listeners depeding on the type of button to control what inputs go to function input, operators, or run the operation.

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
+let currentDisplay = entry1 + " " + operator + " " + entry2 + " = " + result;
 let result = "";
+let operator = ""
+let entry1 = "";
+let entry2 = "";
 
 function operate(num1, num2, operator) {
-    if (operator == "+") {
+    if (operator == "btnPlus") {
         const add = function(num1, num2) {
             result = num1 + num2;
             return result;
         };
-    } else if (operator == "-") {
+    } else if (operator == "btnMinus") {
         const subtract = function(num1, num2) {
             result = num1 - num2;
             return result;
         };
-    } else if (operator == "*") {
+    } else if (operator == "btnMultiply") {
         const multiply = function(num1, num2) {
             result = num1 * num2;
             return result;
         };
-    } else if (operator == "/") {
+    } else if (operator == "btnDivide") {
         const divide = function(num1, num2) {
             result = num1 / num2;
             return result;
@@ -24,6 +28,21 @@ function operate(num1, num2, operator) {
     }
 }
 
+function clickEqual(event) {
+    if (event.target.tagName !== "BUTTON") {
+       return;
+    } 
+    operate();
+
+ }
+
+ function clickOperator(event) {
+    if (event.target.tagName !== "BUTTON") {
+        return;
+     }
+     return operator = event.target.textContent;
+ }
+
 const display = document.querySelector("#display");
-display.textContent = result;
-const equal = document.querySelector("#btnEqual");
+display.textContent = currentDisplay;
+const equal = document.querySelector("#btnEqual").addEventListener("click", clickEqual);
