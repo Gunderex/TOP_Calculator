087be0f323f28b32b9f5da0c62d7bff63e37a110
Gunderex <125839591+Gunderex@users.noreply.github.com>
2023-12-07 14:56:24 -0700

    click handlers added now working through troubleshooting errors with event handlers and variables.

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-let currentDisplay = num1 + " " + lastOperator + " " + num2 + " = " + result;
 let result = "";
 let operator = ""
 let lastOperator = ""
+let lastNum1 = ""
 let num1 = "";
 let num2 = "";
+let currentDisplay = lastNum1 + " " + lastOperator + " " + num2 + " = " + result;
 
 function operate(num1, num2, operator) {
-    if (operator == "btnPlus") {
+    if (num2 == "") {
+        return;
+    } else if (operator == "btnPlus") {
         const add = function(num1, num2) {
             result = num1 + num2;
-            lastOperator = operator
-            operator = ""
+            lastOperator = operator;
+            lastNum1 = num1;
+            num1 = result;
+            operator = "";
             return result;
         };
     } else if (operator == "btnMinus") {
         const subtract = function(num1, num2) {
             result = num1 - num2;
-            lastOperator = operator
-            operator = ""
+            lastOperator = operator;
+            lastNum1 = num1;
+            num1 = result;
+            operator = "";
             return result;
         };
     } else if (operator == "btnMultiply") {
         const multiply = function(num1, num2) {
             result = num1 * num2;
-            lastOperator = operator
-            operator = ""
+            lastOperator = operator;
+            lastNum1 = num1;
+            num1 = result;
+            operator = "";
             return result;
         };
     } else if (operator == "btnDivide") {
         const divide = function(num1, num2) {
             result = num1 / num2;
-            lastOperator = operator
-            operator = ""
+            lastOperator = operator;
+            lastNum1 = num1;
+            num1 = result;
+            operator = "";
             return result;
         };
     }
@@ -55,8 +66,12 @@ function clickEqual(event) {
  function clickNumber(event) {
     if (event.target.tagName !== "BUTTON") {
         return;
-     } else if (operator == "") {
+     } else if (operator == "" && result == "") {
         num1 += ("'" + event.target.textContent + "'");
+     }else if (result !== "") {
+        num1 = result;
+        result = "";
+        num2 += ("'" + event.target.textContent + "'");
      } else if (operator !== "") {
         num2 += ("'" + event.target.textContent + "'");
      }
@@ -65,3 +80,5 @@ function clickEqual(event) {
 const display = document.querySelector("#display");
 display.textContent = currentDisplay;
 const equal = document.querySelector("#btnEqual").addEventListener("click", clickEqual);
+const operatorButton = document.querySelectorAll(".operatorPad").addEventListener("click", clickOperator);
+const numberButton = document.querySelector(".numPad").addEventListener("click", clickNumber);
